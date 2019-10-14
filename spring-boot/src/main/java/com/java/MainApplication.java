621a2004046fbc509f7ffc6e3f50d01ec1b375c1
bob <bookgolden@126.com>
2019-10-15 01:27:18 +0800

    spring boot 升级

--- spring-boot/src/main/java/com/java/MainApplication.java
+++ spring-boot/src/main/java/com/java/MainApplication.java
@@ -1,7 +1,9 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+package com.java;
 
-@EnableAutoConfiguration
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
